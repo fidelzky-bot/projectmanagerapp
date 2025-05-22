f4798b59ff2f333834605b570af1d0dd0517cda5
Fidel <hello.soarmediadigital@gmail.com>
2025-05-23 01:54:31 +0800

    any

--- models/Task.js
+++ models/Task.js
@@ -6,7 +6,9 @@ const TaskSchema = new mongoose.Schema({
   project: { type: mongoose.Schema.Types.ObjectId, ref: 'Project', required: true },
   owner: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   status: { type: String, default: 'To Do' },
-  priority: { type: String, default: 'Low' }
+  priority: { type: String, default: 'Low' },
+  assignedTo: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  dueDate: Date
 });
 
 module.exports = mongoose.model('Task', TaskSchema);
