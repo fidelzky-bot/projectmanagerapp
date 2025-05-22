7bd5d2eee400aca7018b2973d7a1a3c394f6818a
Fidel <hello.soarmediadigital@gmail.com>
2025-05-23 01:45:22 +0800

    add default values for status and priority

--- models/Task.js
+++ models/Task.js
@@ -4,7 +4,9 @@ const TaskSchema = new mongoose.Schema({
   title: String,
   description: String,
   project: { type: mongoose.Schema.Types.ObjectId, ref: 'Project', required: true },
-  owner: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }
+  owner: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  status: { type: String, default: 'To Do' },
+  priority: { type: String, default: 'Low' }
 });
 
 module.exports = mongoose.model('Task', TaskSchema);
