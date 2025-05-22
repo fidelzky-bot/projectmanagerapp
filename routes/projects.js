7312658df2a18e5a63102450dbdc53b4bd28da72
Fidel <hello.soarmediadigital@gmail.com>
2025-05-22 11:14:56 +0800

    Fix test route order in projects.js

--- routes/projects.js
+++ routes/projects.js
@@ -9,6 +9,9 @@ router.post('/', clerkAuth, syncUser, projectController.createProject);
 // Get all projects (public for testing)
 router.get('/', projectController.getProjects);
 // Get a single project by ID
+router.get('/test', (req, res) => {
+  res.json({ message: "Projects test route works!" });
+});
 router.get('/:id', projectController.getProjectById);
 // Update a project
 router.put('/:id', projectController.updateProject);
