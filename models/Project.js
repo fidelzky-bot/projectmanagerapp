1d81134e8d6bc91299bfa9e329c7d8692aa6115a
Fidel <hello.soarmediadigital@gmail.com>
2025-05-22 11:41:48 +0800

    Make owner optional for public project creation

--- models/Project.js
+++ models/Project.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const ProjectSchema = new mongoose.Schema({
   name: { type: String, required: true },
   description: { type: String },
-  owner: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  owner: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: false },
   members: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }]
 }, { timestamps: true });
 
