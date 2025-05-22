3d64ce92e3455c639a921a1bc6ed12b7e532f1ae
Fidel <hello.soarmediadigital@gmail.com>
2025-05-23 07:49:55 +0800

    Am I Updated

--- controllers/inviteController.js
+++ controllers/inviteController.js
@@ -2,6 +2,16 @@ const Invite = require('../models/Invite');
 const Project = require('../models/Project');
 const User = require('../models/User');
 const crypto = require('crypto');
+const nodemailer = require('nodemailer');
+
+// Set up transporter (for Gmail)
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS
+  }
+});
 
 // Create and send an invite
 async function createInvite(req, res) {
@@ -28,6 +38,12 @@ async function createInvite(req, res) {
     // Log the invite link (replace with email sending later)
     const inviteLink = `${process.env.CLIENT_URL || 'http://localhost:3000'}/signup?inviteToken=${token}`;
     console.log('Invite link:', inviteLink);
+    await transporter.sendMail({
+      from: process.env.EMAIL_USER,
+      to: email,
+      subject: 'You are invited to join a project!',
+      html: `<p>You have been invited to join a project. Click <a href="${inviteLink}">here</a> to sign up!</p>`
+    });
     res.status(201).json({ message: 'Invite sent!', inviteLink });
   } catch (err) {
     res.status(500).json({ error: err.message });
