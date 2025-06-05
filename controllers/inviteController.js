50c6e0ebf1ea1500536880a84012a71435b1108c
Fidel <hello.soarmediadigital@gmail.com>
2025-06-05 22:23:22 +0800

    Check

--- controllers/inviteController.js
+++ controllers/inviteController.js
@@ -7,12 +7,13 @@ const Notification = require('../models/Notification');
 
 // Set up transporter (for Gmail)
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: 'smtp.gmail.com',
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
   },
-  secure: true,
   debug: true
 });
 
